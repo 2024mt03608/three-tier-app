d4cfab736dafd4c026d9902aa3808e4d21d6dcac
2024mt03608 <2024mt03608@wilp.bits-pilani.ac.in>
2025-05-03 23:56:34 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,22 @@
+# Outputs the URL of the deployed frontend Cloud Run service
 output "endpoint" {
   description = "The URL of the deployed frontend (Cloud Run service 'fe')"
   value       = google_cloud_run_service.fe.status[0].url
 }
 
+# Outputs the name of the Cloud SQL instance created
 output "sql_instance_name" {
   description = "The name of the Cloud SQL instance created"
   value       = google_sql_database_instance.main.name
 }
 
+# Outputs the resource name of the DB password secret in Secret Manager (only if MySQL is used)
 output "secret_manager_password_secret" {
   description = "The resource name of the Secret Manager secret holding the DB password"
   value       = var.database_type == "mysql" ? google_secret_manager_secret.db_password[0].name : null
 }
 
+# Provides a URL to the GCP Cloud Console walkthrough for deploying this app
 output "in_console_tutorial_url" {
   description = "Link to the GCP Cloud console walkthrough/tutorial for deploying the three-tier app"
   value       = "https://console.cloud.google.com/products/solutions/deployments?walkthrough_id=panels--sic--three-tier-web-app&project=${var.project_id}"
